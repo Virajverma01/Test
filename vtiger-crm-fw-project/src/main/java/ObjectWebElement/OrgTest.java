86fe9dd8707235ec8212fa799682d6b797948407
ABC <ABC@DESKTOP-JB5NLL4>
2025-10-08 13:11:30 -0700

    V-tiger POM

---
+++ vtiger-crm-fw-project/src/main/java/ObjectWebElement/OrgTest.java
@@ -0,0 +1,34 @@
+package ObjectWebElement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrgTest {
+	public OrgTest(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(css = "img[alt='Create Organization...']")
+	private WebElement orgPlusIcon;
+
+	@FindBy(name = "accountname")
+	private WebElement orgField;
+
+	@FindBy(css = "input[title='Save [Alt+S]']")
+	private WebElement saveBtn;
+
+	public WebElement getOrgPlusIcon() {
+		return orgPlusIcon;
+	}
+
+	public WebElement getOrgField() {
+		return orgField;
+	}
+
+	public WebElement getSaveBtn() {
+		return saveBtn;
+	}
+
+}
